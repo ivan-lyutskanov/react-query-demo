544530bbd530fa0a7efbf9b17b0e10ea960c25e7
lyutskanov <ivan.lyutskanov@scalefocus.com>
2022-09-13 11:55:11 +0300

    Add post onSuccess callback

--- src/components/containers/SinglePost/AddPost.js
+++ src/components/containers/SinglePost/AddPost.js
@@ -1,9 +1,15 @@
 import { createPost } from  '../../../services/Posts.service';
+import {messageSuccess } from '../../../utils/notifications';
 
 export default function AddPost({onMutateCb}) {
 
+  const onSuccess = (text) => {
+    onMutateCb && onMutateCb()
+    messageSuccess(text)
+  }
+
   const onCreate = () => createPost({ title: 'New post title', body: 'New post body' })
-                            .then(() => onMutateCb && onMutateCb())
+                            .then(() =>onSuccess('New Post'))
                             .catch((err) => console.error(err))
 
   return <button onClick={onCreate}>+ New Post</button>
